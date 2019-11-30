2f111bdd59fb8ae87427967b19313ceeb95cd772
Zander Mackie <amackie@pivotal.io>
2019-11-30 16:36:28 -0500

    making it

--- doit.sh
+++ doit.sh
@@ -3,5 +3,5 @@
 BP=$1
 DEP=$2
 
-versions=$(cat ~/workspace/${BP}/manifest.yml | yj | jq -r --arg DEP $DEP '.dependencies[] | select(.name == $DEP) | .version')
-echo $versions
+cat ~/workspace/${BP}/manifest.yml | yj | jq -r --arg DEP $DEP '.dependencies[] | select(.name == $DEP) | .version' \
+ | xargs -L 1 -I _ mkdir -p "$DEP/_"
